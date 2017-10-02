950216d65c847d24abe80ccc9348d0ef2f6c5d3a
Md Mahabubul Hasan Masud <masudiiuc@gmail.com>
2017-10-01 19:48:47 -0700

    Update 1.js

--- 1.js
+++ 1.js
@@ -9,27 +9,27 @@
 // Technique 01
 var twoSum = function(nums, target) {
 	var map = new Map();
-  
+
 	for(var i = 0; i < nums.length; i++) {
-    	map.set(nums[i], i);
-    }
+		map.set(nums[i], i);
+	}
     
-    for(var j = 0; j < nums.length; j++) {
+	for(var j = 0; j < nums.length; j++) {
 		var anotherNumber = target - nums[j];
-        if (map.has(anotherNumber) && map.get(anotherNumber) != j) {
-        	return [j, map.get(anotherNumber)];
-        }
-    }
+		if (map.has(anotherNumber) && map.get(anotherNumber) != j) {
+			return [j, map.get(anotherNumber)];
+		}
+	}
 };
 
 // Technique 02
 function twoSum(nums, target) {
-		var i, j;
-		for(i = 0; i < nums.length; i++) {
-    	for(j=0; j < nums.length; j++) {
-      	if (nums[i] == target - nums[j]) {
-        	return [i, j];
-        }
-      }
-    }
+	var i, j;
+	for(i = 0; i < nums.length; i++) {
+    		for(j=0; j < nums.length; j++) {
+			if (nums[i] == target - nums[j]) {
+				return [i, j];
+			}
+	      	}
+	}
 }
